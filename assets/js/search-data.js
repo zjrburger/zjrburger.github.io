9d75aeb6ce3a00a4cac5e8260e0f58b7717d3bfe
zjrburger <148293117+zjrburger@users.noreply.github.com>
2026-06-19 01:53:34 +0000

    Deploying to gh-pages from @ zjrburger/zjrburger.github.io@52033a3279875a8caacd4cfc0e12f418963fa007 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
